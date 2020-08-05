efc51160c10a41219cd4f0ffada40d3b43c4d90b
Pooja S <65026620+pooja1507@users.noreply.github.com>
2020-08-05 22:08:44 +0530

    Add files via upload

---
+++ Sum_commandlineargs.java
@@ -0,0 +1,16 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input1= Integer.parseInt(args[0]);
+		int input2 = Integer.parseInt(args[1]);
+		int result = input1+input2;
+		System.out.println("The sum of "+input1+" and "+input2+" is "+result);
+		
+		
+		
+
+	}
+
+}
